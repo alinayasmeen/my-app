df4f0fec86610765c26bcafe916dba9b7aeb83a5
Hafiza Alina Yasmeen <140942733+alinayasmeen@users.noreply.github.com>
2024-09-25 11:05:32 +0500

    Add files via upload

---
+++ career/page.tsx
@@ -0,0 +1,10 @@
+export default function Career(){
+    return(
+        <div>
+            <h1>Career</h1>
+            <p>
+                At My Company, we strive to provide exceptional careers for our employees. We believe in fostering a diverse and inclusive workforce that values diversity, inclusivity, and equity.
+            </p>
+        </div>
+    )
+}
---
+++ hobby/page.tsx
@@ -0,0 +1,15 @@
+const Hobby = () => {
+    return (
+        <div>
+            <h1>My Hobbies</h1>
+            <ul>
+                <li>Reading</li>
+                <li>Coding</li>
+                <li>Gardening</li>
+            </ul>
+        </div>
+    );
+};
+
+Hobby.displayName = "Hobby"; // Assign a display name for debugging
+export default Hobby; // Export the named function
